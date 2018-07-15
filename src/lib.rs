7d40170553ef9497aa0f0a093c1779b80e8c0b36
Afonso Lage <lage.afonso@gmail.com>
2018-07-15 17:57:36 -0300

    Implemented parse_u32_header function

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate nom;
 mod contact;
 pub use contact::*;
 
+use nom::*;
 use std::str;
 
 pub fn is_param_char(c: u8) -> bool {
@@ -18,14 +19,6 @@ pub fn to_str_default<'a>(s: &'a [u8]) -> &'a str {
     to_str(s).unwrap_or_default()
 }
 
-named!(
-    pub parse_u32<u32>,
-    map_res!(
-        map_res!(nom::digit, std::str::from_utf8),
-        |s: &str| s.parse::<u32>()
-    )
-);
-
 #[derive(PartialEq, Debug)]
 pub struct SingleValueHeader<T> {
     value: T,
@@ -37,13 +30,13 @@ pub enum SipHeader<'a, T> {
     SingleValue(SingleValueHeader<T>),
 }
 
-//named!(
-//    pub parse_u32_header<&str, SingleValueHeader<u32>>,
-//    do_parse!(
-//        v: be_u32
-//        >> (SingleValueHeader { value: v })
-//   )
-//);
+named!(
+    pub parse_u32_header<SingleValueHeader<u32>>,
+    do_parse!(
+        d: take_while!(is_digit)
+            >> (SingleValueHeader { value: str::from_utf8(d).unwrap_or_default().parse::<u32>().unwrap_or_default() })
+   )
+);
 
 //named!(
 //    pub parse_sip_header<&str, (&str, SipHeader)>,
@@ -57,5 +50,5 @@ pub enum SipHeader<'a, T> {
 
 pub fn just_test() {
     //println!("{:#?}", contact::parse_contact("tel:85999684700\r\n"));
-    println!("{:#?}", parse_u32(b"44\r\n"));
+    println!("{:#?}", parse_u32_header(b"44\r\n"));
 }
