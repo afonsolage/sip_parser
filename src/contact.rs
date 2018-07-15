aebb7b63a667e834ed88a15f85c111bd5ed53901
Afonso Lage <lage.afonso@gmail.com>
2018-07-15 17:07:27 -0300

    Changed from &str to &[u8] on contact.rs, to have better compatibility with nom

--- src/contact.rs
+++ src/contact.rs
@@ -1,4 +1,5 @@
 use super::*;
+use std::str;
 
 #[derive(PartialEq, Debug)]
 pub struct Contact<'a> {
@@ -11,7 +12,7 @@ pub struct Contact<'a> {
 }
 
 named!(
-    pub parse_contact<&str, Contact>,
+    pub parse_contact<Contact>,
     do_parse!(
             alias: opt!(
                 alt_complete!(
@@ -20,24 +21,31 @@ named!(
                     //Or until there is a LT
                     take_until!("<") 
                 ))
-            >> take_while_s!(is_space)
+            >> take_while_s!(nom::is_space)
             >> opt!(tag!("<"))
             >> protocol: take_until_and_consume!(":")
             >> extension: take_until_either!("@>;\r\n")
             >> domain: opt!(preceded!(tag!("@"), take_until_either!(":>;\r\n")))
-            >> port: opt!(preceded!(tag!(":"), take_while_s!(is_digit)))
+            >> port: opt!(preceded!(tag!(":"), take_while!(nom::is_digit)))
             >> opt!(tag!(">"))
             >> opt!(tag!(";"))
             >> params: many_till!(
                 //Do those parses
                 do_parse!(
-                    p: take_while_s!(is_param_char) //Save the content on "p"
+                    p: take_while!(is_param_char) //Save the content on "p"
                         >> opt!(tag!(";")) //Remove skip semi-colon
                         >> (p) //Return p and continue
                         
                 //While isn't the end
                 ), tag!("\r\n")) 
-            >> (Contact { alias, protocol, extension, domain, port, params: params.0})
+            >> (Contact {
+                    alias: alias.and_then(to_str),
+                    protocol: to_str_default(protocol),
+                    extension: to_str_default(extension),
+                    domain: domain.and_then(to_str),
+                    port: port.and_then(to_str),
+                    params: params.0.into_iter().filter_map(to_str).collect()
+                })
     )
 );
 
@@ -52,10 +60,10 @@ mod tests {
     fn contact_full() {
         assert_eq!(
             parse_contact(
-                "\"Alice Mark\" <sip:9989898919@127.0.0.1:35436>;tag=asdasdasdasd;some=nice\r\n"
+                b"\"Alice Mark\" <sip:9989898919@127.0.0.1:35436>;tag=asdasdasdasd;some=nice\r\n"
             ),
             Ok((
-                "",
+                b"" as &[u8],
                 Contact {
                     alias: Some("Alice Mark"),
                     protocol: "sip",
@@ -71,9 +79,9 @@ mod tests {
     #[test]
     fn contact_no_alias() {
         assert_eq!(
-            parse_contact("sip:85999684700@localhost\r\n"),
+            parse_contact(b"sip:85999684700@localhost\r\n"),
             Ok((
-                "",
+                b"" as &[u8],
                 Contact {
                     alias: None,
                     protocol: "sip",
@@ -89,9 +97,9 @@ mod tests {
     #[test]
     fn contact_no_host() {
         assert_eq!(
-            parse_contact("tel:+5585999680047\r\n"),
+            parse_contact(b"tel:+5585999680047\r\n"),
             Ok((
-                "",
+                b"" as &[u8],
                 Contact {
                     alias: None,
                     protocol: "tel",
@@ -107,9 +115,9 @@ mod tests {
     #[test]
     fn contact_host_with_port() {
         assert_eq!(
-            parse_contact("sips:mark@localhost:3342\r\n"),
+            parse_contact(b"sips:mark@localhost:3342\r\n"),
             Ok((
-                "",
+                b"" as &[u8],
                 Contact {
                     alias: None,
                     protocol: "sips",
@@ -125,9 +133,9 @@ mod tests {
     #[test]
     fn contact_alias_empty() {
         assert_eq!(
-            parse_contact("<sip:8882@127.0.0.1>\r\n"),
+            parse_contact(b"<sip:8882@127.0.0.1>\r\n"),
             Ok((
-                "",
+                b"" as &[u8],
                 Contact {
                     alias: Some(""),
                     protocol: "sip",
@@ -143,9 +151,9 @@ mod tests {
     #[test]
     fn contact_with_params() {
         assert_eq!(
-            parse_contact("sip:admin@localhost;tag=38298391\r\n"),
+            parse_contact(b"sip:admin@localhost;tag=38298391\r\n"),
             Ok((
-                "",
+                b"" as &[u8],
                 Contact {
                     alias: None,
                     protocol: "sip",
--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,60 @@
 extern crate nom;
 
 mod contact;
+pub use contact::*;
 
-fn is_space(c: char) -> bool {
-    c == ' '
+use std::str;
+
+pub fn is_param_char(c: u8) -> bool {
+    c != b';' && c != b'\r' && c != b'\n'
 }
 
-fn is_digit(c: char) -> bool {
-    c.is_digit(10)
+pub fn to_str<'a>(s: &'a [u8]) -> Option<&'a str> {
+    str::from_utf8(s).ok()
 }
 
-fn is_param_char(c: char) -> bool {
-    c != ';' && c != '\r' && c != '\n'
+pub fn to_str_default<'a>(s: &'a [u8]) -> &'a str {
+    to_str(s).unwrap_or_default()
 }
+
+named!(
+    pub parse_u32<u32>,
+    map_res!(
+        map_res!(nom::digit, std::str::from_utf8),
+        |s: &str| s.parse::<u32>()
+    )
+);
+
+#[derive(PartialEq, Debug)]
+pub struct SingleValueHeader<T> {
+    value: T,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum SipHeader<'a, T> {
+    ContactValue(Contact<'a>),
+    SingleValue(SingleValueHeader<T>),
+}
+
+//named!(
+//    pub parse_u32_header<&str, SingleValueHeader<u32>>,
+//    do_parse!(
+//        v: be_u32
+//        >> (SingleValueHeader { value: v })
+//   )
+//);
+
+//named!(
+//    pub parse_sip_header<&str, (&str, SipHeader)>,
+//    do_parse!(
+//        contact: switch!(take_until_and_consume!(":"),
+//                         "Contact" => parse_contact |
+//                         "Expires" => parse_u32_header)
+//        >> ("", contact)
+//    )
+//);
 
 pub fn just_test() {
-    println!("{:#?}", contact::parse_contact("tel:85999684700\r\n"));
+    //println!("{:#?}", contact::parse_contact("tel:85999684700\r\n"));
+    println!("{:#?}", parse_u32(b"44\r\n"));
 }
